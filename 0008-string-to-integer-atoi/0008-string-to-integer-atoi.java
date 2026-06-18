1aed2369a0e66efb7a1eb26822c89a2489af2e2e
anushkas11 <anushkasharma2599@gmail.com>
2026-06-18 19:51:05 +0530

    Time: 1 ms (100%), Space: 43.4 MB (94.17%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,23 +1,26 @@
 class Solution {
     public int myAtoi(String s) {
-        s=s.trim();
-        if(s.length()==0) return 0;
-        int sign=1;
-        int i=0;
-        if(s.charAt(0)=='-') {
-            sign=-1;
-            i++;
-        }
-        else if(s.charAt(0)=='+') i++;
+       int sign=1,i=0;
+       s=s.trim();
+       if(s.length()==0) return 0;
+       if(s.charAt(i)=='-'){
+        sign=-1;
+        i++;
+       }
+       else if(s.charAt(i)=='+'){
+        sign=1;
+        i++;
+       }
+      
 
-        long num=0;
-        while(i<s.length() && Character.isDigit(s.charAt(i))){
-            num=num*10+(s.charAt(i)-'0');
-            if(num*sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-            if(num*sign< Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            i++;
-        }
-        return (int)(sign*num);
-        
+       long d=0;
+       while(i<s.length() && Character.isDigit(s.charAt(i))){
+        char c=s.charAt(i);
+        d=d*10+(c-'0');
+        if(sign*d>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(sign*d<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        i++;
+       } 
+       return (int)(sign*d);
     }
 }
